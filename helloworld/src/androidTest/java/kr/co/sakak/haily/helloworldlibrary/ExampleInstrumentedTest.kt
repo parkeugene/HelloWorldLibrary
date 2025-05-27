86116abab55fdd20f8ea688aee52a3e2f525bb28
Eugene Park <parkeugene7676@gmail.com>
2025-05-27 20:23:36 +0900

    fix: package dir

--- helloworld/src/androidTest/java/kr/co/sakak/haily/helloworldlibrary/ExampleInstrumentedTest.kt
+++ helloworld/src/androidTest/java/kr/co/sakak/haily/helloworldlibrary/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("kr.co.sakak.haily.helloworldlibrary", appContext.packageName)
+        assertEquals("kr.co.sakak.haily.helloworld.test", appContext.packageName)
     }
 }
